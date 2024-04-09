d7e21ab7592f5db4f378ee6570a052aa821b33dc
Stylehub2005 <artiomlodyata@gmail.com>
2024-04-09 13:32:49 +0300

    inlclude header

--- Sportové oblečenie.php
+++ Sportové oblečenie.php
@@ -14,21 +14,7 @@
 </head>
 
 <body>
-    <header>
-        <a href="index.php"><img class="graficlogo" src="img/logo/logo.png" alt="logo"></a>
-        <input type="checkbox" id="nav-toggle" class="nav-toggle">
-        <nav>
-            <ul>
-                <li><a href="index.php" target="_blank">HOME</a></li>
-                <li><a href="O%20nas.php" target="_blank">BLOG</a></li>
-                <li><a href="Sportové oblečenie.php" target="_blank"> ROZSAH</a></li>
-                <li><a href="reg.php" target="_blank">REGISTRATION</a></li>
-            </ul>
-        </nav>
-        <label for="nav-toggle" class="nav-toggle-label">
-            <span></span>
-        </label>
-    </header>
+<?php include 'parts/header.php'?>
     <main>
         <div class="table-container">
             <table>
