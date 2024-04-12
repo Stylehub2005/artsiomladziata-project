6d071b37e7bb5d8aeb8a611fa1484901f0292c18
Stylehub2005 <artiomlodyata@gmail.com>
2024-04-12 16:22:07 +0300

    header  fix

--- parts/footer.php
+++ parts/footer.php
@@ -1,12 +1,12 @@
 <footer>
     <div class="footer-nav">
-        <a href="index.html">HOME</a>
-        <a href="O nas.html">BLOG</a>
-        <a href="Sportové oblečenie.html">ROZSAH</a>
-        <a href="reg.html">REGISTRATION</a>
+        <a href="index.php">HOME</a>
+        <a href="O nas.php">BLOG</a>
+        <a href="Sportové oblečenie.php">ROZSAH</a>
+        <a href="reg.php">REGISTRATION</a>
     </div>
     <div class="logo">
-        <a href="index.html"><img src="img/logo/logo.png"></a>
+        <a href="index.php"><img src="img/logo/logo.png"></a>
     </div>
     <div class="social">
         <a href="#"><img src="img/image/m_1e61cef7d9.jpg"></a>
--- parts/header.php
+++ parts/header.php
@@ -1,13 +1,13 @@
 
 <header>
-        <a href="index.html"><img class="graficlogo" src="img/logo/logo.png" alt="logo"></a>
+        <a href="index.php"><img class="graficlogo" src="img/logo/logo.png" alt="logo"></a>
         <input type="checkbox" id="nav-toggle" class="nav-toggle">
         <nav>
             <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="./O nas.html" target="_blank">BLOG</a></li>
-                <li><a href="./Sportové oblečenie.html" target="_blank"> ROZSAH</a></li>
-                <li><a href="./reg.html" target="_blank">REGISTRATION</a></li>
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="./O nas.php" target="_blank">BLOG</a></li>
+                <li><a href="./Sportové oblečenie.php" target="_blank"> ROZSAH</a></li>
+                <li><a href="./reg.php" target="_blank">REGISTRATION</a></li>
             </ul>
         </nav>
         <label for="nav-toggle" class="nav-toggle-label">
