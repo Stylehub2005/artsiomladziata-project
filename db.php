2cc0ccdda31c41d2888be7c406827e13d0ece528
Stylehub2005 <artiomlodyata@gmail.com>
2024-05-13 22:25:56 +0300

    test

--- db.php
+++ db.php
@@ -10,43 +10,49 @@ $db = new PDO("mysql:host=$dbhost; dbname=$dbname", $username, $password);
 
 function get_albums() {
     global $db;
-    $albums = $db->query("SELECT * FROM albums");
+    $albums = $db->query("SELECT * FROM carousel");
     return $albums;
 }
 
 
 function get_singles() {
     global $db;
-    $singles = $db->query("SELECT * FROM singles");
+    $singles = $db->query("SELECT * FROM index.banner");
     return $singles;
 }
 
 function get_videos(){
     global $db;
-    $videos = $db->query("SELECT * FROM videos");
+    $videos = $db->query("SELECT * FROM klient_sales.rozsah");
     return $videos;
 }
 
 function get_career(){
     global $db;
-    $career = $db->query("SELECT * FROM career");
+    $career = $db->query("SELECT * FROM nas_blog.blog");
     return $career;
 }
 
 function get_music_styles(){
     global $db;
-    $music_style = $db->query("SELECT * FROM music_style");
+    $music_style = $db->query("SELECT * FROM reg.reg");
     return $music_style;
 }
 
 function get_info_table(){
     global $db;
-    $info_table = $db->query("SELECT * FROM info_table");
+    $info_table = $db->query("SELECT * FROM rozsah.rozsah");
     return $info_table;
 }
 function get_banners(){
     global $db;
-    $banners = $db->query("SELECT * FROM banners");
+    $banners = $db->query("SELECT * FROM sale.rozsah");
+    return $banners;
+}
+
+function subscribe_blog(){
+    global $db;
+    $banners = $db->query("SELECT * FROM subscribe.blog");
     return $banners;
 }
 
