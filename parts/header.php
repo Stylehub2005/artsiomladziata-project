f009b971964bae5b09fe776229e7d92351d41553
Stylehub2005 <artiomlodyata@gmail.com>
2024-06-01 19:17:51 +0300

    fix header.php

--- parts/header.php
+++ parts/header.php
@@ -1,4 +1,4 @@
-<?php require_once 'db.php'?>
+<?php include_once('db.php'); ?>
 <header>
         <a href="index.php"><img class="graficlogo" src="img/logo/logo.png" alt="logo"></a>
         <input type="checkbox" id="nav-toggle" class="nav-toggle">
@@ -8,6 +8,7 @@
                 <li><a href="./O nas.php" target="_blank">BLOG</a></li>
                 <li><a href="./Sportové oblečenie.php" target="_blank"> ROZSAH</a></li>
                 <li><a href="./reg.php" target="_blank">REGISTRATION</a></li>
+                <li><a href="./view.php" target="_blank">CRUD</a></li>
             </ul>
         </nav>
         <label for="nav-toggle" class="nav-toggle-label">
